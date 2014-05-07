710f76c1af4ae893d4bb151394346c610133e379
Viktor <viktor>
2014-05-07 22:43:06 +0200

    http curl request error handling and a bit better logging

--- step.sh
+++ step.sh
@@ -6,9 +6,23 @@ echo "MAILGUN_SEND_TO: $MAILGUN_SEND_TO"
 echo "MAILGUN_EMAIL_SUBJECT: $MAILGUN_EMAIL_SUBJECT"
 echo "MAILGUN_EMAIL_MESSAGE: $MAILGUN_EMAIL_MESSAGE"
 
-curl -v --user "api:$MAILGUN_API_KEY" \
+
+res=$(curl -is --user "api:$MAILGUN_API_KEY" \
   https://api.mailgun.net/v2/$MAILGUN_DOMAIN/messages \
   -F from="Concrete Mailgun Step <postmaster@$MAILGUN_DOMAIN>" \
   -F to="$MAILGUN_SEND_TO" \
   -F subject="$MAILGUN_EMAIL_SUBJECT" \
-  --form-string html="$MAILGUN_EMAIL_MESSAGE"
+  --form-string html="$MAILGUN_EMAIL_MESSAGE")
+
+echo " --- Result ---"
+echo "$res"
+echo " --------------"
+
+http_code=$(echo "$res" | grep HTTP/ | awk {'print $2'} | tail -1)
+echo " [i] http_code: $http_code"
+
+if [ "$http_code" == "200" ]; then
+  exit 0
+else
+  exit 1
+fi
